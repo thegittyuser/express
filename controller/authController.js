0a6efc3ab299dd2508e3f9f53d45cf638a88ca95
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2026-07-23 18:54:32 +0500

    Login & DoLogin

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,11 @@
+import { capitalCase } from "change-case";
+
 const users = [];
 
 export const register = (req, res) => {
   res.render("register");
 };
+
 export const doregister = (req, res) => {
   const { username, email, password } = req.body;
 
@@ -16,4 +19,26 @@ export const doregister = (req, res) => {
   users.push({ username, email, password });
   console.log(users);
   res.redirect("login");
+};
+
+export const login = (req, res) => {
+  res.render("login");
+};
+
+export const dologin = (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((u) => u.email === email);
+  if (!user) {
+    res.send("Email does not exist");
+  }
+
+  if (user.password !== password) {
+    res.send("Password is incorrect.");
+  }
+
+
+  user.username = capitalCase(user.username);
+
+  res.render("welcome", { user, title: "Welcome" });
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { doregister, register } from "../controller/authController.js";
+import {
+  dologin,
+  doregister,
+  login,
+  register,
+} from "../controller/authController.js";
 const router = express.Router();
 
 router.get("/register", register);
 router.post("/doregister", doregister);
+router.get("/login", login);
+router.post("/dologin", dologin);
 
 export default router;
